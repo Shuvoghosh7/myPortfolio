634eefd0a7c7d21fd89948411bbd027feae210f4
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-08-06 13:21:36 +0600

    add download resume option

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './About.css';
 import img from '../../Assets/myProfile.png';
-
+import { FaFacebookF } from 'react-icons/fa';
+import { FaLinkedinIn } from 'react-icons/fa';
+import { BsGithub } from 'react-icons/bs';
 import Typewriter from 'typewriter-effect';
+
+import resume from './Resume Of Shuvo Gosh.pdf'
 const About = () => {
     return (
         <div className='mt-24 about lg:mx-28 mx-5'>
@@ -11,22 +15,36 @@ const About = () => {
                     <h1 className='text-[#FF2E59] text-5xl font-sans-serif'>Hello! I'm</h1>
                     <p className='name-title'>Shuvo Gosh</p>
                     <p className='text-3xl mb-3 font-sans-serif'>
-                    <Typewriter
+                        <Typewriter
                             options={{
-                                strings: ['React Developer','Front-End Develover', 'MERN Stack Develover'],
+                                strings: ['React Developer', 'Front-End Develover', 'MERN Stack Develover'],
                                 autoStart: true,
                                 loop: true,
                             }}
                         />
                     </p>
-                    <p className='resume'>Download Resume</p>
+                    <a href={resume} download='resume'>
+                        <p className='resume'>Download Resume</p>
+                    </a>
 
                 </div>
-                <div>
-                    <img className='lg:w-[400px]' src={img} alt="" />
+                <div className='flex justify-center items-center'>
+                    <div>
+                        <img className='lg:w-[400px]' src={img} alt="" />
+                    </div>
+                    <div>
+                        <p className='icon bg-[#2B4865] p-2'>
+                            <a href="https://github.com/Shuvoghosh7" target="blank"><BsGithub className='icons git' /></a>
+                        </p>
+                        <p className='icon mt-5 bg-[#2B4865] p-2'>
+                            <a href="https://www.linkedin.com/in/shuvogosh/" target="blank"> <FaLinkedinIn className='icons li' /></a></p>
+                        <p className='icon mt-5 bg-[#2B4865] p-2'>
+                            <a href="https://www.facebook.com/shuvo.gosh.5" target="blank"> <FaFacebookF className='icons fb' /> </a>
+                        </p>
+                    </div>
                 </div>
             </div>
-            
+
         </div>
     );
 };
